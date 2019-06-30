6d04b3fc6081cbdbc5efb8a7f21278a4f07df550
JlFerrete <joseluisferrete@gmail.com>
2019-06-30 12:41:31 +0200

    gitflow-feature-stash: first_steps
    Add Avatars and buttons

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, Avatar } from "react-native-elements";
 
 export default function App() {
+  buttonOnClick = value => {
+    console.log("Click en mi primer botón: " + value);
+  };
+
   return (
     <View style={styles.container}>
+      <Avatar
+        rounded
+        size='large'
+        source={{
+          uri: "https://api.adorable.io/avatars/285/abott@adorable.png"
+        }}
+      />
       <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Mi primer Botón"
+        onPress={() => this.buttonOnClick("Hola Mundo")}
+      />
     </View>
   );
 }
@@ -12,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
